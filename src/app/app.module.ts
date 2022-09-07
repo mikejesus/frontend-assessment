f2f88412eb6a70e81a84d5a8d8e3c7fb0d7f9c18
Michael Olawuni <michael.olawuni@ubagroup.com>
2022-09-07 20:27:41 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { SplashScreenComponent } from './core/splash-screen/splash-screen.component';
+import { HeroSectionComponent } from './features/hero-section/hero-section.component';
+import { AboutSectionComponent } from './features/about-section/about-section.component';
+import { ProductListComponent } from './features/product-list/product-list.component';
+import { ProductCardComponent } from './shared/product-card/product-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SplashScreenComponent,
+    HeroSectionComponent,
+    AboutSectionComponent,
+    ProductListComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
